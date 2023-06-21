0bfb9e82033aa785f372a3ef4ea61a0ef97b866f
Tomoki Miyauchi <development.operation.6.6.6@gmail.com>
2023-06-21 12:50:29 +0900

    docs(mod): add description

--- mod.ts
+++ mod.ts
@@ -1,4 +1,8 @@
 // Copyright © 2023 Tomoki Miyauchi. All rights reserved. MIT license.
 // This module is browser compatible.
 
+/** Numeric sequence lazy generator, TC39 [proposal-iterator.range](https://github.com/tc39/proposal-iterator.range) implementation.
+ * @module
+ */
+
 export { type NumericRangeOptions, range } from "./range.ts";
